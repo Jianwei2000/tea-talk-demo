138e0bc9c04d180c30dba0e057ca71bfffbadfdf
Jianwei2000 <er891129@gmail.com>
2024-05-22 09:40:31 +0800

    Update project with new changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory('/tea-talk-demo/'),
   routes: [
     //前台
     {
